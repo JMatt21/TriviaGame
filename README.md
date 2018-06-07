671e2a0e166d021d20fa17b14c9aebf51a8288ec
JustMatt <MatthewBertrandt@gmail.com>
2018-06-07 07:59:46 -0700

    Final

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TriviaGame
+
+TriviaGame is a little game based on several trivia questions on music.
+It's not really useful, it's a game that has the same questions asked over and over again.
+However, it is useful in that it is very modular. Any question can be easily added with any number of answers.
+
+I currently maintain this project on github: https://github.com/JMatt21/TriviaGame
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
 //Questions
 var questionArray = [
     question("Fill in the blank. ___ Man by Billy Joel.", ["Band", "Fat", "Short", "Piano"], "Piano",
-                "The correct answer was Piano. Billy Joel is neither fat or short, but he was in a band."),
+        "The correct answer was Piano. Billy Joel is neither fat or short, but he was in a band."),
     question("Country music is also known as...", ["African Music", "Texas Music", "Uncle Farmer Bob's Favorite", "Hillbilly Music"], "Hillbilly Music",
-                "The correct answer was Hillbilly Music. Uncle Farmer Bob really liked metal."),
+        "The correct answer was Hillbilly Music. Uncle Farmer Bob really liked metal."),
     question("Which century was Jazz born in?", ["14th", "20th", "1st", "18th", "19th"], "19th",
-                "19th century, because my opinion is F A C T."),
+        "19th century, because my opinion is F A C T."),
     question("Who was in the band Tenacious D?", ["Tim Allen", "Jack Black"], "Jack Black",
-                'Tim Allen starred in "Home Improvement". It was okay, I guess. Never did music.'),
+        'Tim Allen starred in "Home Improvement". It was okay, I guess. Never did music.'),
     question("Which of these do NOT belong?", ["Tuba", "Trumpet", "Cornet", "Saxophone"], "Saxophone",
-                "A saxophone might look all brass-like from a distance and also made of metal, but it is considered a woodwind instrument. Likely because it uses a reed."),
+        "A saxophone might look all brass-like from a distance and also made of metal, but it is considered a woodwind instrument. Likely because it uses a reed."),
     question("Which one of these intruments does not have a mute?", ["Xylophone", "Tuba", "Trumpet", "Cello"], "Xylophone",
-                "The correct answer was Xylophone. Tuba's and Trumpet's can be stuffed and a Cello has this weird rubber atttachment."),
+        "The correct answer was Xylophone. Tuba's and Trumpet's can be stuffed and a Cello has this weird rubber atttachment."),
     question("True or False? The Beatles had a member called Stuart Moore", ["True", "False"], "False",
-                "Stuart Sutcliffe and Tommy Moore were both members. Not their combined amalgamation."),
+        "Stuart Sutcliffe and Tommy Moore were both members. Not their combined amalgamation."),
     question("Which instrument would you least likely find in a jazz band?", ["Drumset", "Vibes", "Saxophone", "Trombone"], "Vibes",
-                "Vibes are not as common as the classical jazzical three.")
+        "Vibes are not as common as the classical jazzical three.")
 ]
 var currentQuestion;
 //variables
@@ -23,7 +23,7 @@ var numberCorrect = 0, numberIncorrect = 0, numberUnanswered = 0;
 var intervalID;
 var backgroundMusic = new Audio("assets/audio/Flying Home.mp3");
 //Timer
-const MAX_TIME = 15;
+const MAX_TIME = 13;
 var timer = {
     time: MAX_TIME,
     start: function () {
@@ -56,7 +56,6 @@ function question(theQuestion, options, answer, wrongAnswerText) {
     var ret = {
         theQuestion: theQuestion,
         options: randomizeArray(options),
-        length: options.length,
         answer: answer,
         wrongText: wrongAnswerText,
 
@@ -66,10 +65,10 @@ function question(theQuestion, options, answer, wrongAnswerText) {
             $("#timer").text(timer.time);
             timer.start();
             currentQuestion = this;
-            console.log(currentQuestion);
+            // console.log(currentQuestion);
             $("#question").text(this.theQuestion);
             this.options.forEach(function (opt) {
-                createButton(opt, "help");
+                createButton(opt, "option");
             });
         },
 
@@ -104,18 +103,8 @@ function randomizeArray(array) {
 
 function createButton(text, bClass) {
     var newDiv = $("<div>");
-    newDiv.html("<button class=" + bClass + ">" + text + "</button>");
+    newDiv.html('<button class="' + bClass + '">' + text + "</button>");
     $("#content").append(newDiv);
-}
-
-function startGame() {
-    backgroundMusic.play();
-    backgroundMusic.loop = true;
-    $("#time").show();
-    console.log("Starting Game.");
-    $("#content").empty();
-    console.log("Emptying Content.");
-    questionArray[0].display();
 }
 
 function imageAndResponseTransistion() {
@@ -125,24 +114,19 @@ function imageAndResponseTransistion() {
 }
 //Main
 $(document).ready(function () {
-
     $("#time").hide();
-    createButton("hello there", "startGame");
+    createButton("Start", "startGame");
 
-    $(".startGame").on("click", function () {
-        startGame();
-    });
-
-    $("#content").on("click", ".help", function () {
+    $("#content").on("click", ".option", function () {
         var input = $(this).text();
         if (input === currentQuestion.answer) {
-            console.log(input + " is Correct.");
+            // console.log(input + " is Correct.");
             numberCorrect++;
             imageAndResponseTransistion();
-            $("#content").empty().append("<img src='assets/images/right.png' width = 200 height = 200>")
-                .append("<p>Great Job! Onto the next one!</p>");
+            $("#content").empty().append("<img src='assets/images/right.jpg' width = 200 height = 200>")
+                .append("<p>Great Job!</p>");
         } else {
-            console.log(input + " is Wrong.");
+            // console.log(input + " is Wrong.");
             numberIncorrect++;
             imageAndResponseTransistion();
             $("#content").empty().append("<img src='assets/images/wrong.png' width = 200 height = 200>")
@@ -153,16 +137,21 @@ $(document).ready(function () {
             currentQuestion.toNext();
         }, 3000);
 
-
     }).on("click", ".restart", function () {
-        console.log("reset");
+        // console.log("reset");
         $("#content").empty();
         numberCorrect = 0, numberIncorrect = 0, numberUnanswered = 0;
+        createButton("Start", "startGame");
+        backgroundMusic.pause();
+
+    }).on("click", ".startGame", function () {
+        backgroundMusic.play();
+        backgroundMusic.volume = 0.1;
+        backgroundMusic.loop = true;
+        $("#time").show();
+        // console.log("Starting Game.");
+        $("#content").empty();
+        // console.log("Emptying Content.");
         questionArray[0].display();
     });
-
-
-
-
-
 });
